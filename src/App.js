2656142a2718918231f9dca77b820ade678eeaac
Chase Gomez <gomechas@flip1.engr.oregonstate.edu>
2022-07-16 14:21:31 -0700

    Verified React app.  Added copywrites to HomePage.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer className="App-footer">
+        <p>Powered by:</p>
+        <p>© 2022 | Chase Gomez</p>
+      </footer>
     </div>
   );
 }
