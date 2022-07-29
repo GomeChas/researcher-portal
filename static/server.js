fa827f6e15ed66c4b775ef5813ac874e766be6c4
Chase Gomez <gomechas@flip1.engr.oregonstate.edu>
2022-07-29 14:54:39 -0700

    handlebars is finally running the server

--- static/server.js
+++ static/server.js
@@ -4,33 +4,32 @@ const express = require("express");
 const app = express();
 const PORT = 9727;
 
-//Database
-//var db = require('./db-connection');
-
-//Handlebars
-//var exphbs = require('express-handlebars');
-//const { query } = require('express');
-//app.engine('.hbs', exphbs ({
-    //extname: '.hbs'
-//}));
-//app.set('view engine', '.hbs');
-
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(express.static('public'));
 
+//Database
+var db = require('./database/db-connector');
+
+//Handlebars
+const handlebars = require('express-handlebars');
+const { query } = require('express');
+app.engine('handlebars', handlebars.engine());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
 // Note: Don't add or change anything above this line.
 
-//app.get('/researchers', function(req, res) {
-    //let read_query = 'SELECT * FROM Researchers;';
-    //db.pool.query(read_query, function(errors, rows, fields) {
-        //res.render('researchers', {data: rows});
-    //})
-//});
+app.get('/researchers', function(req, res) {
+    let r_query = 'SELECT * FROM Researchers;';
+    db.pool.query(r_query, function(errors, rows, fields) {
+        res.render('researchers', {data: rows});
+    })
+});
 
-// Note: Don't add or change anything below this line.
+// Note: Don't add o  r change anything below this line.
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...press Ctrl-C to terminate`);
 });
