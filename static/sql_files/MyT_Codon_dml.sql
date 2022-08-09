430071ed70c574b881c9a6636e9e75ea1e5c8628
Jonah Tang <tangjon@oregonstate.edu>
2022-08-08 17:47:08 -0700

    Updated DML

--- static/sql_files/MyT_Codon_dml.sql
+++ static/sql_files/MyT_Codon_dml.sql
@@ -1,7 +1,11 @@
--- List all LabNotebooks on the home page with the associated Researchers' names
+-- MyT Codon DML
+-- The colon : character is used to denote variables (typically user inputs)
+
+
+-- LABNOTEBOOKS/PROJECTSTAFF PAGE --------------------------------------------------
+
+-- READ LabNotebooks table without a MitoGene filter applied
 SELECT
-PS.ProjectStaffID,
-CONCAT(R.FirstName," ", R.LastName) AS FullName,
 LN.LabNotebookID,
 LN.SpecialProjectName,
 LN.CreationDate,
@@ -13,17 +17,80 @@ COALESCE(LN.CompletionDate,'N/A') AS CompletionDate,
 COALESCE(LN.StorageFreezer,'N/A') AS StorageFreezer,
 COALESCE(LN.FreezerBoxLoc,'N/A') AS FreezerBoxLoc
 FROM LabNotebooks LN
-    LEFT JOIN ProjectStaff PS
-        ON PS.LabNotebookID = LN.LabNotebookID
-    LEFT JOIN Researchers R
-        ON R.ResearcherID = PS.ResearcherID
-;
--- List distinct SpecialProjectNames for selection --
+ORDER BY LN.SpecialProjectName ASC;
+
+-- READ LabNotebooks table with a MitoGene filter applied
+SELECT
+LN.LabNotebookID,
+LN.SpecialProjectName,
+LN.CreationDate,
+CASE
+    WHEN LN.TransfectionComplete = 1 THEN 'Yes'
+    ELSE 'No'
+END AS TransfectionComplete,
+COALESCE(LN.CompletionDate,'N/A') AS CompletionDate,
+COALESCE(LN.StorageFreezer,'N/A') AS StorageFreezer,
+COALESCE(LN.FreezerBoxLoc,'N/A') AS FreezerBoxLoc
+FROM LabNotebooks LN
+    JOIN Chimeras C
+        ON C.LabNotebookID = LN.LabNotebookID
+    JOIN MitoGenes MG
+        ON MG.MitoGeneID = C.MitoGeneID
+        AND MG.HgncSymbol = :inputHgncSymbol
+ORDER BY LN.SpecialProjectName ASC;
+
+-- READ ProjectStaff table
+SELECT
+PS.ProjectStaffID,
+LN.LabNotebookID,
+LN.SpecialProjectName,
+R.ResearcherID,
+CONCAT(R.FirstName," ", R.LastName) AS FullName
+FROM ProjectStaff PS 
+JOIN LabNotebooks LN
+    ON LN.LabNotebookID = PS.LabNotebookID
+JOIN Researchers R
+    ON R.ResearcherID = PS.ResearcherID
+ORDER BY LN.SpecialProjectName ASC,
+FullName ASC;
+
+-- READ MitoGene names for ProjectStaff dropdown
 SELECT DISTINCT
-SpecialProjectName
-FROM LabNotebooks
-;
--- List all Researchers on the Researchers page
+HgncSymbol
+FROM MitoGenes;
+
+-- READ Researcher names for ProjectStaff dropdown
+SELECT
+ResearcherID,
+CONCAT(FirstName," ", LastName) AS FullName
+FROM Researchers;
+
+-- CREATE LabNotebook entry where Completion is complete
+INSERT INTO LabNotebooks (SpecialProjectName, CreationDate, TransfectionComplete, CompletionDate, StorageFreezer, FreezerBoxLoc)
+VALUES
+(:inputSpecialProjectName, NOW(), :inputTransfectionComplete, NOW(), :inputStorageFreezer, :inputFreezerBoxLoc);
+
+-- CREATE LabNotebook entry where Completion is not complete
+INSERT INTO LabNotebooks (SpecialProjectName, CreationDate, TransfectionComplete, CompletionDate, StorageFreezer, FreezerBoxLoc)
+VALUES
+(:inputSpecialProjectName, NOW(), :inputTransfectionComplete, NULL, :inputStorageFreezer, :inputFreezerBoxLoc);
+
+-- CREATE a new ProjectStaff entry with the associated Researcher when a LabNotebook entry is created
+INSERT INTO ProjectStaff (ResearcherID, LabNotebookID)
+VALUES
+(:inputNewProjectPI, (SELECT MAX(LabNotebookID) FROM LabNotebooks));
+
+-- CREATE a new ProjectStaff entry, associating a Researcher with a LabNotebook 
+INSERT INTO ProjectStaff (ResearcherID, LabNotebookID)
+VALUES
+(:inputResearcherID, :inputLabNotebookID);
+
+-- DELETE a ProjectStaff entry
+DELETE FROM ProjectStaff WHERE ResearcherID = :inputResearcherID AND LabNotebookID = :inputLabNotebookID;
+
+-- RESEARCHERS PAGE --------------------------------------------------
+
+-- READ Researchers table
 SELECT
 ResearcherID,
 FirstName,
@@ -32,153 +99,111 @@ CASE
     WHEN Credential = 1 THEN 'Yes'
     ELSE 'No'
 END AS Credential
-FROM Researchers
-;
--- List all MitoGenes on the Genes page
+FROM Researchers;
+
+-- CREATE Researcher entry
+INSERT INTO Researchers (FirstName, LastName, Credential)
+VALUES
+(:inputFirstName, :inputLastName, :inputCredential);
+
+-- UPDATE an existing Researcher
+UPDATE Researchers 
+SET FirstName = :inputFirstName, 
+LastName = :inputLastName, 
+Credential = :inputCredential 
+WHERE ResearcherID = :inputResearcherID;
+
+-- DELETE a Researcher, which also deletes its associated ProjectStaff entries
+DELETE FROM ProjectStaff WHERE ResearcherID = :inputResearcherID;
+DELETE FROM Researchers WHERE ResearcherID = :inputResearcherID;
+
+-- CHIMERAS PAGE --------------------------------------------------
+
+-- READ Chimeras table 
 SELECT
-MitoGeneID,
-HgncID,
-HgncSymbol,
-HgncName,
-COALESCE(NCBIGeneID, 'N/A') AS NCBIGeneID,
-COALESCE(UniProtID, 'N/A') AS UniProtID
-FROM MitoGenes
-;
--- List all Vectors on the Vectors page with the AntiBacterial name
-SELECT 
-V.VectorID,
-V.ProductName, 
-V.AntiBacterialID,
-AB.AntiBacterialName, 
-VectorSize, 
-RECutSites 
-FROM Vectors V 
-    INNER JOIN AntiBacterials AB 
-        ON AB.AntiBacterialID = V.AntiBacterialID
-;
--- List all Chimeras on the Chimeras page
-SELECT
+C.ChimeraID,
 LN.LabNotebookID,
-LN.SpecialProjectName,
+COALESCE(LN.SpecialProjectName, 'N/A') AS SpecialProjectName,
 MG.MitoGeneID,
-MG.HgncSymbol,
+COALESCE(MG.HgncSymbol, 'N/A') AS HgncSymbol,
 V.VectorID,
-V.ProductName,
-ProviderName,
-DiseaseName
+COALESCE(V.ProductName, 'N/A') AS ProductName,
+COALESCE(ProviderName, 'N/A') AS ProviderName,
+COALESCE(DiseaseName, 'N/A') AS DiseaseName
 FROM Chimeras C
     INNER JOIN MitoGenes MG
         ON MG.MitoGeneID = C.MitoGeneID
     INNER JOIN Vectors V
         ON V.VectorID = C.VectorID
-    INNER JOIN LabNotebooks LN
+    LEFT JOIN LabNotebooks LN
         ON LN.LabNotebookID = C.LabNotebookID
-;
--- List all AntiBacterials on the AntiBacterials page
+ORDER BY SpecialProjectName ASC;
+
+-- READ LabNotebook SpecialProjectName for dropdown menu
+SELECT DISTINCT
+LabNotebookID,
+SpecialProjectName
+FROM LabNotebooks;
+
+-- READ MitoGene attributes for dropdown menu
+SELECT * FROM MitoGenes;
+
+-- READ Vector attributes for dropdown menu
+SELECT * FROM Vectors;
+
+-- CREATE new Chimera entry
+INSERT INTO Chimeras (LabNotebookID, MitoGeneID, VectorID, ProviderName, DiseaseName)
+VALUES
+(:inputLabNotebookID, :inputMitoGeneID, :inputVectorID, :inputProviderName, :inputDiseaseName);
+
+-- UPDATE LabNotebook FK of a Chimera (nullable)
+UPDATE Chimeras SET Chimeras.LabNotebookID = :inputLabNotebookID WHERE ChimeraID = :inputChimeraID;
+
+-- MITOGENES PAGE --------------------------------------------------
+
+-- READ MitoGenes table
 SELECT
-*
-FROM AntiBacterials
-;
+MitoGeneID,
+HgncID,
+COALESCE(HgncSymbol, 'N/A') AS HgncSymbol,
+COALESCE(HgncName, 'N/A') AS HgncName,
+NCBIGeneID,
+COALESCE(UniProtID, 'N/A') AS UniProtID
+FROM MitoGenes;
 
--- For the following INSERT functionalities, the colon : character will be used to denote variables
+-- CREATE a new MitoGene entry
+INSERT INTO MitoGenes (HgncID, HgncSymbol, HgncName, NCBIGeneID, UniProtID) 
+VALUES 
+(:inputHgncID, :inputHgncSymbol, :inputHgncName, :inputNCBIGeneID, :inputUniProtID);
 
--- Create a new LabNotebook --------------------
-INSERT INTO LabNotebooks(
-    SpecialProjectName,
-    CreationDate,
-    TransfectionComplete,
-    CompletionDate,
-    StorageFreezer,
-    FreezerBoxLoc
-)
+-- VECTORS PAGE --------------------------------------------------
+
+-- READ Vectors table
+SELECT 
+V.VectorID,
+V.ProductName, 
+V.AntiBacterialID,
+COALESCE(AB.AntiBacterialName, 'N/A') AS AntiBacterialName,
+COALESCE(VectorSize, 'N/A') AS VectorSize,
+COALESCE(RECutSites, 'N/A') AS RECutSites
+FROM Vectors V 
+    LEFT JOIN AntiBacterials AB 
+        ON AB.AntiBacterialID = V.AntiBacterialID;
+
+-- READ AntiBacterials for dropdown menu
+SELECT * FROM AntiBacterials;
+
+-- CREATE a new Vector entry
+INSERT INTO Vectors (ProductName, AntiBacterialID, VectorSize, RECutSites)
 VALUES
-(:SpecialProjectName, :currentDate, 0, NULL, :storageFreezerInput, :freezerBoxLocInput);
+(:inputProductName, :inputAntiBacterialID, :inputVectorSize, :inputRECutSites);
 
--- When creating a new LabNotebook, a ProjectStaff entry will be created for each staff member selected
-INSERT INTO ProjectStaff (
-    ResearcherID,
-    LabNotebookID
-)
+-- ANTIBACTERIALS PAGE --------------------------------------------------
+
+-- READ AntiBacterials table
+SELECT * FROM AntiBacterials;
+
+-- CREATE new AntiBacterial entry
+INSERT INTO AntiBacterials (AntiBacterialName)
 VALUES
-(:researcherIDSelection, :newLabNotebookID);
-
--- Create a new Researcher --------------------
-INSERT INTO Researchers (
-    FirstName,
-    LastName,
-    Credential
-)
-VALUES
-(:firstNameInput, :lastNameInput, :credentialInput);
-
--- Create a new Gene --------------------
-INSERT INTO MitoGenes (
-    HgncID,
-    HgncSymbol,
-    HgncName,
-    NCBIGeneID,
-    UniProtID
-)
-VALUES
-(:hgncIDInput, :hgncSymbolInput, :hgncNameInput, :NCBIGeneIDInput, :UniProtID);
-
--- Create a new Vector --------------------
-INSERT INTO Vectors (
-    ProductName,
-    AntiBacterialID,
-    VectorSize,
-    RECutSites
-)
-VALUES
-(:productNameInput, :antiBacterialIDSelection, :vectorSizeInput, :reCutSitesInput);
-
--- Create a new Chimera --------------------
-INSERT INTO Chimeras (
-    LabNotebookID,
-    MitoGeneID,
-    VectorID,
-    ProviderName,
-    DiseaseName
-)
-VALUES
-(:newLabNotebookID, :mitoGeneSelection, :vectorSelection, :providerNameInput, :diseaseNameInput);
-
--- Create a new AntiBacterial --------------------
-INSERT INTO AntiBacterials (
-    AntiBacterialName
-)
-VALUES
-(:antiBacterialNameInput);
-
--- Update an existing LabNotebook ----------------
-UPDATE LabNotebooks
-SET TransfectionComplete = :transfectionCompleteSelection,
-    CompletionDate = :currentDateIfComplete,
-    StorageFreezer = :storageFreezerSelection,
-    FreezerBoxLoc = :freezerBoxLocInput
-WHERE LabNotebookID = :activeLabNotebookID;
-
--- If new staff is added to the project
-INSERT INTO ProjectStaff (
-    ResearcherID,
-    LabNotebookID
-)
-VALUES
-(:researcherIDSelection, :activeLabNotebookID);
-
--- Delete an existing Researcher --------------------
-DELETE FROM Researchers
-    WHERE ResearcherID = :selectedResearcherID;
-
--- Filter ProjectStaff by Project --------------------
-SELECT
-PS.ProjectStaffID,
-LN.SpecialProjectName,
-CONCAT(R.FirstName," ", R.LastName) AS FullName
-FROM ProjectStaff PS
-    JOIN LabNotebooks LN
-        ON LN.LabNotebookID = PS.LabNotebookID
-    JOIN Researchers R
-        ON R.ResearcherID = PS.ResearcherID
-WHERE LN.SpecialProjectName = :selectedSpecialProjectName
-ORDER BY FullName ASC;
+(:inputAntiBacterialName)
